f1f06d4da3c3d8301cbea87a2089765f9c485bdc
seogineer <dgseo8981@gmail.com>
2022-09-04 15:47:01 +0900

    불필요한 소스 제거

--- src/main/java/com/seogineer/demooauthspringboot/common/security/SecurityConfig.java
+++ src/main/java/com/seogineer/demooauthspringboot/common/security/SecurityConfig.java
@@ -7,14 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-    private final SecurityFilter securityFilter;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
